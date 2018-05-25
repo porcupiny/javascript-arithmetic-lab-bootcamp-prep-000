fc483a431e194547fd017f0071be2299d8c9c9cd
Liane Chan <liane.chan@gmail.com>
2018-05-25 23:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var a, b;
-
 var a = Math.floor(Math.random() * 1000);
-  b = Math.floor(Math.random() * 1000);
+var b = Math.floor(Math.random() * 1000);
   
   function add(a, b) {
     return a + b;
