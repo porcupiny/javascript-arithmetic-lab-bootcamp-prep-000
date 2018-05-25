683ad8383b14451c4e94256c537a8b7e0cd7ecb3
Liane Chan <liane.chan@gmail.com>
2018-05-25 23:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ var a, b;
   function add(a, b) {
     return a + b;
   }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
