24989e882382f3e5fad3f55ceccb62e90b8d4646
Liane Chan <liane.chan@gmail.com>
2018-05-25 23:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var b = Math.floor(Math.random() * 1000);
     return a-1;
   }
   
-var n = Math.floor(Math.random() * 1000);
+var n = Math.random();
 
   function makeInt(n) {
     return parseInt('n', 10);
