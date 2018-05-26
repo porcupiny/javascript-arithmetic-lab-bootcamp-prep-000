bb44905d065a0f95eb7738ae0486c035e64a0649
Liane Chan <liane.chan@gmail.com>
2018-05-26 00:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ var b = Math.floor(Math.random() * 1000);
 var n;
 
   function makeInt(n) {
-    return parseInt('n', 10);
+    return parseInt(n, 10);
   }
   
   function preserveDecimal(n) {
-    return parseFloat('n');
+    return parseFloat(n);
   }
